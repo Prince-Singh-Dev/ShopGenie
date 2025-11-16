8b1aaa1efb48be504344e59090aa94479c2c877e
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-11-16 19:31:12 +0530

    Design and Implementing Add Product Page design

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { addProduct } from '../controller/productController.js'
+import upload from '../middleware/multer.js'
 
 let productRoutes = express.Router()
 
-productRoutes.post("/addproduct",upload.fields([
+productRoutes.post("/addproduct", upload.fields([
     {name:"image1",maxCount : 1},
     {name:"image2",maxCount : 1},
     {name:"image3",maxCount : 1},
