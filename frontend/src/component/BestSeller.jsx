3ad8cb2d644145f26a0365af175bfd1dacf77336
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-11-24 16:12:23 +0530

    Completed Home Page with Latest Collections and Best Sellers

--- frontend/src/component/BestSeller.jsx
+++ frontend/src/component/BestSeller.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Title from './Title'
+import { shopDataContext } from '../context/ShopContext'
+import Card from './Card'
 
 function BestSeller() {
+  let {products} = useContext(shopDataContext)
+  let [bestSeller , setBestSeller] = useState([])
+
+  useEffect(()=>{
+    let filterProduct = products.filter((item)=>item.bestseller)
+    setBestSeller(filterProduct.slice(0,4));
+  },[products])
+
   return (
-    <div></div>
+    <div>
+
+      <div className='h-[8%] w-[100%] text-center mt-[50px]'>
+        <Title text1={"BEST"} text2={"SELLER"}/>
+        <p className='w-[100%] m-auto text-[13px] md:text-[20px] px-[10px] text-blue-100'>Tried , Testes , Loved Discover Our All-Time Best Sellers</p>
+      </div>
+
+      <div className='w-[100%] h-[50%] mt-[30px] flex items-center jusitfy-center flex-wrap gap-[50px]'>
+        {
+          bestSeller.map((item,index) => (
+            <Card key={index} name={item.name} image={item.image1} id={item._id} price={item.price}/>
+          ))
+        }
+      </div>
+
+    </div>
   )
 }
 
