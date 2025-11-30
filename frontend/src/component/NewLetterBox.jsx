5f7243209bdcd6792b029d629bd3be529a1c1ed6
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-11-30 14:18:48 +0530

    Fixed minor updates in home page cover photo

--- frontend/src/component/NewLetterBox.jsx
+++ frontend/src/component/NewLetterBox.jsx
@@ -8,7 +8,7 @@ function NewLetterBox() {
 
   return (
     <div className='w-[100%] h-[40vh] bg-gradient-to-b from-[#141414] to-[#0c2025] flex items-center justify-start gap-[10px] flex-col'>
-        <p className='md:text-[30px] text-[20px] text-[#a5faf7] font-semibold px-[20px] mt-[12px]'>Subscribe now & get 20% off</p>
+        <p className='md:text-[30px] text-[20px] text-[#a5faf7] font-semibold px-[20px] mt-[12px]'>Subscribe now & get 30% off</p>
 
         <p className='md:text-[18px] text-[14px] text-center text-blue-100 font-semibold px-[20px]'>Subscribe now and enjoy exclusive savings , special deals and early access to new collections.</p>
 
