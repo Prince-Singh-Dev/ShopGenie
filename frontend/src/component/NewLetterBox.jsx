9b2bb2b5b3968acf714fcba17d3d21c407aa656c
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-11-30 14:11:33 +0530

    Completed the designing of About us page with Some few minor updates in NewLetterBox section

--- frontend/src/component/NewLetterBox.jsx
+++ frontend/src/component/NewLetterBox.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function NewLetterBox() {
 
-    const handleSubmit = ()=>{
+    const handleSubmit = (e)=>{
         e.preventDefault()
     }
 
   return (
     <div className='w-[100%] h-[40vh] bg-gradient-to-b from-[#141414] to-[#0c2025] flex items-center justify-start gap-[10px] flex-col'>
-        <p className='md:text-[30px] text-[20px] text-[#a5faf7] font-semibold px-[20px]'>Subscribe now & get 20% off</p>
+        <p className='md:text-[30px] text-[20px] text-[#a5faf7] font-semibold px-[20px] mt-[12px]'>Subscribe now & get 20% off</p>
 
-        <p className='md:text-[18px] text-[14px] text-center text-blue-100 font-semiboldpx-[20px]'>Subscribe now and enjoy exclusive savings , special deals and early access to new collections.</p>
+        <p className='md:text-[18px] text-[14px] text-center text-blue-100 font-semibold px-[20px]'>Subscribe now and enjoy exclusive savings , special deals and early access to new collections.</p>
 
         <form action="" onSubmit={handleSubmit} className='w-[100%] h-[30%] md:h-[50%] flex items-center justify-center mt-[20px] gap-[20px] px-[20px]'>
             <input type="text" placeholder='Enter Your Email' className='placeholder:text-[black] bg-slate-300 w-[600px] max-w-[60%] h-[40px] px-[20px] rounded-lg shadow-sm shadow-black' required/>
