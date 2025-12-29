374f81e21dcb006a76b123df72af84af67a99350
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-12-29 14:31:38 +0530

    Completed Order routes setup

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cors from "cors"
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config()
 let port = process.env.PORT || 6000
 
@@ -24,6 +25,8 @@ app.use("/api/auth",authRoutes)
 app.use("/api/user",userRoutes)
 app.use("/api/product",productRoutes)
 app.use("/api/cart",cartRoutes)
+app.use("/api/order",orderRoutes)
+
 
 app.listen(port,()=>{
     console.log("Hello from Server")
