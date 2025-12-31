b4e3f322dad4efa0b83f61435598b6460f7d60a5
Prince Singh <princesingh6173@gmail.com>
2025-12-31 18:22:09 +0530

    Update CORS origin to include admin frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : ["https://shopgenie-frontend.onrender.com" , "http://localhost:5174"],
+    origin : ["https://shopgenie-frontend.onrender.com" , "https://shopgenie-admin.onrender.com"],
     credentials : true
 }))
 
